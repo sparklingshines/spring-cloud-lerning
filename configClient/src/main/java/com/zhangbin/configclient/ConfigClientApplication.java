34e3f217bf2093f3037e0a809e2ab57584b6b5f1
sparklingshines <langyanhn@163.com>
2018-01-22 18:02:46 +0800

    Add files via upload

---
+++ configClient/src/main/java/com/zhangbin/configclient/ConfigClientApplication.java
@@ -0,0 +1,26 @@
+package com.zhangbin.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@Value("${neo.hello}")
+	private String hello;
+
+	@RequestMapping("/hello")
+	public String from() {
+		return this.hello;
+	}
+}
