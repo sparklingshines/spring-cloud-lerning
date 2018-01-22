2d22f97ee10dfa6714cbe4304785fe668283b402
sparklingshines <langyanhn@163.com>
2018-01-22 18:03:34 +0800

    Add files via upload

---
+++ eurekaclient/src/main/java/com/zhangbin/eurekaclient/EurekaClientApplaction.java
@@ -0,0 +1,27 @@
+package com.zhangbin.eurekaclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplaction {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplaction.class, args);
+	}
+	
+	@Value("${server.port}")
+    String port;
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
+
+}
